c2a944d742f02026beacd20571974ac1235fa506
Jenovi Wayela <wjenovi@gmail.com>
2023-09-19 17:04:36 +0200

    removed home

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     # ..
   devise_for :users, controllers: { registrations: 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
   # Defines the root path route ("/")
 
   resources :experiences, only: [:index, :new, :create, :show]
