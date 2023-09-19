0f7956c10631fc9bc4e2d0f29e5177608b17cc2f
Carine Rugorirwera <carinerugorirwera@gmail.com>
2023-09-19 17:56:01 +0200

    Added dates to bookings model and updated new_bookings form, started creating cards for experiences index page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,15 +5,16 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.user = current_user
     if @booking.save
-      redirect_to @booking, notice: "Booking was succesfully created."
+      redirect_to dashboard_path, notice: "Booking was succesfully created."
     else
       render :new, alert: :unprocessable_entity
     end
   end
 
   def booking_params
-    params.require(:booking).permit(:status)
+    params.require(:booking).permit(:status, :start_date, :end_date, :experience_id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_123444) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_153405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_123444) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["experience_id"], name: "index_bookings_on_experience_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
