88675c4b40ba00b3483ab94d59a647f50964664d
Jenovi Wayela <wjenovi@gmail.com>
2023-09-20 16:30:11 +0200

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     # ...
   get 'dashboard', to: 'dashboard#index', as: :dashboard
     # ..
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
 
